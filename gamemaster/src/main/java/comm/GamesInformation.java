6e42c49b6187414c68854fbc693e1f702efbd643
Björn Hansson <bjorn@bjornhansson.se>
2016-11-18 11:12:32 +0100

    Gson dependency and REST API example

---
+++ gamemaster/src/main/java/comm/GamesInformation.java
@@ -0,0 +1,13 @@
+package comm;
+
+public class GamesInformation {
+	private String test;
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+}
