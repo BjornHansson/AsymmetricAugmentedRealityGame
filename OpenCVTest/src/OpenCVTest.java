58cafa05488f7e84736b5383baaf5e9df5714135
Alex Baldwin <alexander.baldwin@mah.se>
2016-11-24 11:36:05 +0100

    Got object tracking example code to work

--- OpenCVTest/src/OpenCVTest.java
+++ OpenCVTest/src/OpenCVTest.java
@@ -3,16 +3,28 @@ import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.FrameGrabber.Exception;
+import org.bytedeco.javacv.JavaCV;
 import org.bytedeco.javacv.OpenCVFrameConverter;
 
 import static org.bytedeco.javacpp.opencv_core.*;
 import static org.bytedeco.javacpp.opencv_imgcodecs.*;
 import static org.bytedeco.javacpp.opencv_imgproc.*;
 
+import org.bytedeco.javacpp.opencv_core;
+
 public class OpenCVTest {
 	
 	static CvScalar rgba_min = cvScalar(0, 0, 0, 0);// RED wide dabur birko
     static CvScalar rgba_max = cvScalar(255, 255, 255, 255);
+    
+    static int iLowH = 170;
+	static int iHighH = 179;
+
+	static int iLowS = 150;
+	static int iHighS = 255;
+
+	static int iLowV = 60;
+	static int iHighV = 255;
 
 	public static void main(String[] args) {	
 
@@ -22,19 +34,57 @@ public class OpenCVTest {
 		try {
 			grabber.start();
 			OpenCVFrameConverter.ToIplImage converter = new OpenCVFrameConverter.ToIplImage();
-			IplImage grabbedImage = converter.convert(grabber.grab());
+			OpenCVFrameConverter.ToMat matConverter = new OpenCVFrameConverter.ToMat();
+			//IplImage grabbedImage = converter.convert(grabber.grab());
+			Mat grabbedImage = matConverter.convert(grabber.grab());
+			IplImage grabbedImage2 = converter.convert(grabber.grab());
 			
 			CanvasFrame frameOriginal = new CanvasFrame("Original", CanvasFrame.getDefaultGamma()/grabber.getGamma());
 			CanvasFrame frameProcessed = new CanvasFrame("Processed", CanvasFrame.getDefaultGamma()/grabber.getGamma());
 			
-			while (frameOriginal.isVisible() && (grabbedImage = converter.convert(grabber.grab())) != null) {
+			while (frameOriginal.isVisible() && (grabbedImage = matConverter.convert(grabber.grab())) != null) {
 				
-				IplImage imgThreshold = cvCreateImage(cvGetSize(grabbedImage), 8, 1);
+				Mat imgHSV = new Mat();
+
+				cvtColor(grabbedImage, imgHSV, COLOR_BGR2HSV);
+				
+				Mat imgThresholded = imgHSV.clone();
+
+				//IplImage imgHSV = grabbedImage2.clone();
+				//cvCvtColor(grabbedImage2,imgHSV,CV_BGR2HSV );
+				
+				//IplImage imgThresholded = imgHSV.clone();
+				
+				
+				Scalar lowScalar = RGB(iLowH, iLowS, iLowV);
+				Scalar highScalar = RGB(iHighH, iHighS, iHighV);
+				Mat lowMat = new Mat(4,1,imgHSV.type(),new Scalar());
+				Mat highMat = new Mat(4,1,imgHSV.type(),new Scalar());
+			    //inRange(imgHSV, lowMat, highMat, imgThresholded); //Threshold the image
+			
+			    
+				
+			    //cvInRangeS(grabbedImage, rgba_min, rgba_max, imgThresholded);
+				
+			    
+			    
+				//morphological opening (removes small objects from the foreground)
+				erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, new Size(5, 5)));
+				dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, new Size(5, 5)));
+
+				//morphological closing (removes small holes from the foreground)
+				dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, new Size(5, 5)));
+				erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, new Size(5, 5)));
+				
+				//IplImage test = new IplImage()
+				//cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV);
+				
+				//IplImage imgThreshold = cvCreateImage(cvGetSize(grabbedImage), 8, 1);
 		        //
-		        cvInRangeS(grabbedImage, rgba_min, rgba_max, imgThreshold);// red
+		       // cvInRangeS(grabbedImage, rgba_min, rgba_max, imgThreshold);// red
 		        //cvSmooth(imgThreshold, imgThreshold, CV_MEDIAN, 15,0,0,0);
 		        
-		        Frame fP = converter.convert(imgThreshold);
+		        Frame fP = matConverter.convert(imgThresholded);
 				frameProcessed.showImage(fP);
 				
 				/*Mat imgHSV;
@@ -77,7 +127,7 @@ public class OpenCVTest {
 					iLastY = posY;
 				}*/
 				
-				Frame f = converter.convert(grabbedImage);
+				Frame f = matConverter.convert(grabbedImage);
 				frameOriginal.showImage(f);
 			
 			}
