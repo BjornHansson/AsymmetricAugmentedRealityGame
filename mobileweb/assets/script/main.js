fd4f74c8f6f459685ca44d96c3869e31b229b7f1
koddas <johan@idioti.se>
2016-12-01 11:40:21 +0100

    Slightly solidified RequireJS stuff.

--- mobileweb/assets/script/main.js
+++ mobileweb/assets/script/main.js
@@ -1,3 +1,11 @@
-/**
- * http://usejsdoc.org/
- */
+requirejs.config({
+    baseUrl: 'assets/script/lib',
+    paths: {
+        jquery: 'jquery-3.1.1.min',
+        bootstrap: 'bootstrap-3.3.7.min'
+    },
+    waitSeconds: 2,
+    bundles: {
+        'game_logic': ['../game_logic/game_control', '../game_logic/game', '../game_logic/player']
+    }
+});
