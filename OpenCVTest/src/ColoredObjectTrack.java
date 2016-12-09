6a7793d1b64d78ed31f1e9d6fd40bcb7ac1af415
warriv93 <warriv93@gmail.com>
2016-12-09 13:25:27 +0100

    save color values state to file almost done

--- OpenCVTest/src/ColoredObjectTrack.java
+++ OpenCVTest/src/ColoredObjectTrack.java
@@ -25,7 +25,13 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -46,6 +52,10 @@ import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.FrameGrabber;
 import org.bytedeco.javacv.Java2DFrameConverter;
 import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.swing.JButton;
 
 
 
@@ -71,6 +81,7 @@ public class ColoredObjectTrack implements Runnable {
 	 * Correct the color range- it depends upon the object, camera quality,
 	 * environment.
 	 */
+	
 	int a = 0, b = 0, c = 0, d = 100, e = 255, f = 255;
 
 	CvScalar rgba_min = cvScalar(0, 0, 0, 0);
@@ -109,6 +120,14 @@ public class ColoredObjectTrack implements Runnable {
 	int ii = 0;
 
 	public void ColoredObjectTrackcontrolinterface() {
+		
+		a = 150;
+		b = 50;
+		c = 0;
+		d = 100;
+		e = 255;
+		f = 255;
+
 		aCameraControll = new CameraControll();
 		canvas.addKeyListener(aCameraControll);
 		thresholdedCanvas.addKeyListener(aCameraControll);
@@ -116,14 +135,14 @@ public class ColoredObjectTrack implements Runnable {
 
 		JPanel panel = new JPanel();
 		canvas2.getContentPane().add(panel, BorderLayout.WEST);
-		panel.setLayout(new GridLayout(6, 1, 0, 0));
+		panel.setLayout(new GridLayout(7, 1, 0, 0));
 
 		JLabel lblRedMinimum = new JLabel("Red minimum");
 		panel.add(lblRedMinimum);
 
 		JPanel panel_1 = new JPanel();
 		canvas2.getContentPane().add(panel_1, BorderLayout.EAST);
-		panel_1.setLayout(new GridLayout(6, 1, 0, 0));
+		panel_1.setLayout(new GridLayout(7, 1, 0, 0));
 
 		JLabel lblRedMaximum = new JLabel("Red maximum");
 		panel.add(lblRedMaximum);
@@ -144,6 +163,7 @@ public class ColoredObjectTrack implements Runnable {
 		slider.setMaximum(255);
 		lblRedMinimum.setLabelFor(slider);
 		panel_1.add(slider);
+		slider.setValue(a);
 		slider.addChangeListener(new ChangeListener() {
 
 			public void stateChanged(ChangeEvent e) {
@@ -157,6 +177,7 @@ public class ColoredObjectTrack implements Runnable {
 		slider_1.setMaximum(255);
 		lblRedMaximum.setLabelFor(slider_1);
 		panel_1.add(slider_1);
+		slider_1.setValue(d);
 		slider_1.addChangeListener(new ChangeListener() {
 
 			public void stateChanged(ChangeEvent e) {
@@ -170,6 +191,7 @@ public class ColoredObjectTrack implements Runnable {
 		slider_2.setMaximum(255);
 		lblGreenMinimum.setLabelFor(slider_2);
 		panel_1.add(slider_2);
+		slider_2.setValue(b);
 		slider_2.addChangeListener(new ChangeListener() {
 
 			public void stateChanged(ChangeEvent e) {
@@ -183,6 +205,7 @@ public class ColoredObjectTrack implements Runnable {
 		slider_3.setMaximum(255);
 		lblGreenMaximum.setLabelFor(slider_3);
 		panel_1.add(slider_3);
+		slider_3.setValue(e);
 		slider_3.addChangeListener(new ChangeListener() {
 
 			public void stateChanged(ChangeEvent event) {
@@ -196,6 +219,7 @@ public class ColoredObjectTrack implements Runnable {
 		slider_4.setMaximum(255);
 		lblBlueMinimum.setLabelFor(slider_4);
 		panel_1.add(slider_4);
+		slider_4.setValue(c);
 		slider_4.addChangeListener(new ChangeListener() {
 
 			public void stateChanged(ChangeEvent e) {
@@ -209,7 +233,57 @@ public class ColoredObjectTrack implements Runnable {
 		slider_5.setMaximum(255);
 		slider_5.setBorder(new EmptyBorder(0, 4, 0, 0));
 		lblBlueMaximum.setLabelFor(slider_5);
+		
+		JButton btnSaveState = new JButton("Save state");
+		btnSaveState.setToolTipText("Save the current slider value state");
+		panel.add(btnSaveState);
+		btnSaveState.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent event) {
+				// TODO Auto-generated method stub
+				JSONObject obj = new JSONObject();
+				obj.put("a", a);
+				obj.put("d", d);
+				obj.put("b", b);
+				obj.put("e", e);
+				obj.put("c", c);
+				obj.put("f", f);
+
+				System.out.println("obj in string:   " + obj.toString());
+				
+				
+				
+//				 try-with-resources statement based on post comment below :)
+				try {
+					File file = new File("/home/simon/file1.txt");
+					file.createNewFile();
+					//FileWriter wfile = new FileWriter("~/Documents/file1.txt");
+					System.out.println(file);
+					
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+					System.out.println("Problem");
+				}
+//				FileReader readFile = new FileReader();
+				
+//					try {
+//						file.write(obj.toString());
+//						System.out.println("Successfully Copied JSON Object to File...");
+//						System.out.println("\nJSON Object: " + obj);
+//					} catch (IOException e2) {
+//						e2.printStackTrace();
+//					
+//					} finally {
+//						file.flush();
+//						file.close();
+//					}
+				
+			}
+		});
+
 		panel_1.add(slider_5);
+		slider_5.setValue(f);
 		slider_5.addChangeListener(new ChangeListener() {
 
 			public void stateChanged(ChangeEvent e) {
