ce871a991b2f7356dc530f6e5e7ab47d16896b43
koddas <johan@idioti.se>
2016-12-15 11:46:07 +0100

    Implemented the visual effects.

--- mobileweb/assets/scripts/view_logic/effects.js
+++ mobileweb/assets/scripts/view_logic/effects.js
@@ -1,13 +1,63 @@
-define(function() {
+define(['jquery'], function($) {
     var Effects = function() {};
+
+    /**
+     * Show the defuse failure message.
+     */
+    Effects.prototype.defuseFailure = function() {
+        label = $('#disarm_fail');
+        popup(label);
+    };
+
+    /**
+     * Show the defuse success message.
+     */
+    Effects.prototype.defuseSuccess = function() {
+        label = $('#disarm_success');
+        popup(label);
+    };
+
+    /**
+     * Fade between two views.
+     * 
+     * @param from - The name of the view that will be hidden
+     * @param to - The name of the view that will be shown
+     */
+    Effects.prototype.fadeBetween = function(from, to) {
+        from = $('#' + from);
+        to = $('#' + to);
+
+        from.fadeOut(300, function() {
+            to.css('display', 'none').removeClass('hidden').fadeIn(300);
+        });
+    };
+
+    /**
+     * Initialize the countdown display.
+     * 
+     * @param timer - The number of seconds that remain
+     */
+    Effects.prototype.countdown = function(timer) {
+        var label = $('#countdown');
+
+        label.text(timer);
+
+        var intervalId = setInterval(function() {
+            if (--timer >= 0) {
+                label.text(timer);
+            } else {
+                clearInterval(intervalId);
+            }
+        }, 1000);
+    };
     
-    Effects.prototype.defuseFailure = function() {};
-    
-    Effects.prototype.defuseSuccess = function() {};
-    
-    Effects.prototype.fadeBetween = function(from, to) {};
-    
-    Effects.prototype.countdown = function(time) {};
-    
+    function popup(o) {
+        o.css('display', 'none').removeClass('hidden').fadeIn(300, function(){
+            setTimeout(function() {
+                o.fadeOut(300);
+            }, 800);
+        });
+    }
+
     return Effects;
 });
