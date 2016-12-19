5b3c515e2b2b8ba6703433d494c2fd03f0066213
koddas <johan@idioti.se>
2016-12-19 14:18:41 +0100

    Added update interval support.

--- mobileweb/assets/scripts/config.js
+++ mobileweb/assets/scripts/config.js
@@ -1,6 +1,7 @@
 define(function() {
     var config = {
-            serverAddress: 'https://private-497f9-aarg.apiary-mock.com'
+            serverAddress: 'https://private-497f9-aarg.apiary-mock.com',
+            updateInterval: 1000
     };
     
     return config;
