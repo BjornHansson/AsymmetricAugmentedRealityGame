a2bf1d4638bbb2c13ffcfa953502a44abc698c39
koddas <johan@idioti.se>
2016-12-20 09:29:53 +0100

    Added Underscore to the mix.

--- mobileweb/assets/scripts/main.js
+++ mobileweb/assets/scripts/main.js
@@ -2,9 +2,12 @@ requirejs.config({
     baseUrl: 'assets/scripts',
     paths: {
         config: 'config',
+        proxy: 'proxy',
         jquery: 'lib/jquery-3.1.1.min',
+        underscore: 'lib/underscore-1.8.3.min',
         bootstrap: 'lib/bootstrap-3.3.7.min',
         effects: 'view_logic/effects',
+        flow: 'flow',
         game: 'game_logic/game',
         player: 'game_logic/player',
         game_controls: 'game_logic/game_controls'
@@ -12,18 +15,7 @@ requirejs.config({
     waitSeconds: 2
 });
 
-requirejs(['config', 'jquery', 'effects', 'game_controls'],
-        function(config, $, Effects, GameControls) {
-    var effects = new Effects();
-    var gameControls = new GameControls(config.serverAddress);
-    console.log(config);
-    console.log($);
-    console.log(effects);
-    console.log(gameControls);
+requirejs(['config', 'jquery', 'effects', 'flow', 'game_controls'],
+        function(config, $, Effects, Flow, GameControls) {
     
-    $('#join_game_button').click(function() { effects.fadeBetween('welcome_view', 'defuse_view'); });
-    $('#disarm_button').click(function() { effects.defuseSuccess() });
-    effects.countdown(30);
-    gameControls.listGames().done(function (data) { console.log(data) });
-    gameControls.createGame({ name: 'lalal' }).done(function (data) { console.log(data) });
 });
